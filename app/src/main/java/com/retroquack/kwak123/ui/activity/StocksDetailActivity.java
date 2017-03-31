f0e9dd20284cb41edddf2d2bdc424331fab6d641
kwak123 <kwak123@gmail.com>
2017-03-31 18:31:44 -0400

    Start refactor

---
+++ app/src/main/java/com/retroquack/kwak123/ui/activity/StocksDetailActivity.java
@@ -0,0 +1,8 @@
+package com.retroquack.kwak123.ui.activity;
+
+/**
+ * Created by kwak123 on 3/31/2017.
+ */
+
+public class StocksDetailActivity {
+}
---
+++ app/src/main/java/com/retroquack/kwak123/ui/activity/StocksMainActivity.java
@@ -0,0 +1,10 @@
+package com.retroquack.kwak123.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kwak123 on 3/31/2017.
+ */
+
+public class StocksMainActivity extends AppCompatActivity {
+}
